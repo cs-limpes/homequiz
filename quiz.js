94e1018c25ba0a0ebd100fe0890066fb910be5fe
EddieSavageOG <educatedsavage@educatedsavage.com>
2023-09-14 14:02:15 -0700

    updated with the actual quiz questions and answers, changed to radio buttons and added a function to make them persistent

--- quiz.js
+++ quiz.js
@@ -1,134 +1,64 @@
 // quiz.js
 
 let currentQuestionIndex = 0;
-let scores = [0, 0, 0, 0, 0]; // Assuming each answer corresponds to one of the five results
+let scores = [0, 0, 0, 0]; // Assuming each answer corresponds to one of the five results
 
 const questions = [
     {
-        question: "Your first question here?",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
+        question: "How soon do you want to move?",
+        answers: ["I need to move like yesterday",
+         "I want to move in before Christmas", 
+         "I am dreaming now but will move when I find exactly what I am looking for"],
+        score: [0, 1, 2] // This is just an example. Adjust scores as needed.
     },
     {
-      question: "Your second question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your third question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 4th question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 5th question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 6th question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 7th question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 8th question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 9th question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 10 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 11 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 12 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 13 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 14 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 15 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 16 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 17 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 18 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 19 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 20 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 21 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 22 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 23 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 24 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] // This is just an example. Adjust scores as needed.
-  },
-  {
-      question: "Your 25 question here?",
-      answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-      score: [0, 1, 2, 3] 
-  }
+        question: "What's your design influence?",
+        answers: ["I don't really care and I want someone else to handle all the details", 
+        "I have some ideas, but could use some help making sure they all work together", 
+        "I should have been a designer and I want to choose every finish from house color to bathroom tile"],
+        score: [0, 1, 2] // This is just an example. Adjust scores as needed.
+    },
+    {
+        question: "What kind of home plan is appealing to you?",
+        answers: ["I know how many rooms I need and leave the layout to the professionals", 
+        "I have some specific needs but I’m not sure where to start", 
+        "I am one of a kind and my home design needs to reflect that"],
+        score: [0, 1, 2] // This is just an example. Adjust scores as needed.
+    },
+    {
+        question: "How big is your dream yard?",
+        answers: ["I don't care for yard work, give me something small", 
+        "I need room to roam, but something I can maintain ", 
+        "I’m starting a farm, I want something huge!"],
+        score: [0, 1, 2] // This is just an example. Adjust scores as needed.
+    },
+    {
+        question: "What style of community do you see yourself in?",
+        answers: ["I enjoy having neighbors close by, and bring on the block parties!", 
+        "I am flexible on where I live to get the home I want.", 
+        "I don’t want to see my neighbors"],
+        score: [0, 1, 2] // This is just an example. Adjust scores as needed.
+    },
+    {
+        question: "What is your budget like?",
+        answers: ["I have no budget for my dream home.", "I’ve worked really hard for this home and I know what I want but I have a budget", 
+        "Have you seen this economy? Lets keep costs down"],
+        score: [2, 1, 0] // This is just an example. Adjust scores as needed.
+    },
+    {
+        question: "How do you feel about an HOA?",
+        answers: ["I would never move into an HOA", 
+        "I’d be curious to see what an HOA can do for me", 
+        "I LOVE how nice HOA communities look!"],
+        score: [2, 1, 0] // This is just an example. Adjust scores as needed.
+    },
+    {
+        question: "How involved would you like to be in building your home?",
+        answers: ["I’ll oversee every detail!", 
+        "I want to stay informed but leave the decisions to the expert", 
+        "Please don't bore me with this."],
+        score: [2, 1, 0] // This is just an example. Adjust scores as needed.
+    }
     
 ];
 
@@ -137,8 +67,12 @@ function renderQuestion() {
     let html = `<h2>${question.question}</h2>`;
     
     question.answers.forEach((answer, index) => {
-        html += `<button onclick="selectAnswer(${index})">${answer}</button>`;
+        let isChecked = index === scores[currentQuestionIndex] ? "checked" : "";
+        html += `<input type="radio" name="answer" value="${index}" id="answer${index}" ${isChecked}>`;
+        html += `<label for="answer${index}">${answer}</label><br>`;
     });
+    
+    html += `<button onclick="selectAnswer()">Next</button>`;
 
     if (currentQuestionIndex > 0) {
         html += '<button onclick="previousQuestion()">Previous</button>';
@@ -147,8 +81,15 @@ function renderQuestion() {
     document.getElementById('quiz-container').innerHTML = html;
 }
 
-function selectAnswer(answerIndex) {
-    scores[currentQuestionIndex] = questions[currentQuestionIndex].score[answerIndex];
+
+function selectAnswer() {
+    const selectedRadio = document.querySelector('input[name="answer"]:checked');
+    if (!selectedRadio) {
+        alert("Please select an answer!");
+        return;
+    }
+    const answerIndex = parseInt(selectedRadio.value);
+    scores[currentQuestionIndex] = answerIndex;
     
     currentQuestionIndex++;
     if (currentQuestionIndex < questions.length) {
@@ -160,6 +101,7 @@ function selectAnswer(answerIndex) {
     }
 }
 
+
 function previousQuestion() {
     if (currentQuestionIndex > 0) {
         currentQuestionIndex--;
@@ -170,21 +112,33 @@ function previousQuestion() {
 function submitQuiz() {
     // Here, you'd send the scores and user info to your server or CRM
     // For now, we'll just calculate the result and alert it
-    const totalScore = scores.reduce((a, b) => a + b, 0);
+    const totalScore = scores.map((answerIndex, questionIndex) => questions[questionIndex].score[answerIndex]).reduce((a, b) => a + b, 0);
+
     let result = "Unknown"; // Default result
 
-    if (totalScore < 15) {
-        result = "Original Barbie";
-    } else if (totalScore < 30) {
-        result = "Malibu Barbie";
-    } else if (totalScore < 45) {
-            result = "Fashion Fever Barbie";
-      } else if (totalScore < 60) {
-            result = "Silkstone Barbie";
-      } else {
-            result = "Barbie Signature";
-      };
+    if (totalScore <= 4) {
+        result = "Quick Move In Homes";
+    } else if (totalScore <= 8) {
+        result = "Semi-Custom Homes";
+    } else if (totalScore <= 12) {
+        result = "Custom Homes";
+    } else {
+        result = "On Your Lot";
+    };
+    
     alert(`Hello ${document.getElementById('name').value}, your result is: ${result}`);
+}
+
+function resetQuiz() {
+    console.log("resetting...")
+    currentQuestionIndex = 0;
+    scores.fill(0); // Reset all scores to 0
+
+    // Show the quiz container and hide the user info form (if it's visible)
+    document.getElementById('quiz-container').style.display = 'block';
+    document.getElementById('user-info').style.display = 'none';
+
+    renderQuestion(); // Render the first question
 }
 
 // Start the quiz
